631b894457970f5ae7ce003185311ee04a77523b
Anton Podviaznikov <podviaznikov@gmail.com>
2011-08-13 16:15:54 +0200

    more minor updates.

--- src/views.js
+++ src/views.js
@@ -1,38 +1,37 @@
 /**(c) 2011 Enginimation Studio (http://enginimation.com). May be freely distributed under the MIT license.*/
 /*global Backbone: true,$:true */
-Backbone.BasicView=Backbone.View.extend({
-    hide:function(){
-        this.$(this.el).hide();
-        return this;
-    },
-    show:function(){
-        this.$(this.el).show();
-        return this;
-    },
-    html:function(html){
-        this.$(this.el).html(html);
-        return this;
-    }
-});
+Backbone.BasicView=Backbone.View.extend({});
+Backbone.BasicView.prototype.hide=function(){
+    this.$(this.el).hide();
+    return this;
+};
+Backbone.BasicView.prototype.show=function(){
+    this.$(this.el).show();
+    return this;
+};
+Backbone.BasicView.prototype.html=function(html){
+    this.$(this.el).html(html);
+    return this;
+};
+
 // view for single model
-Backbone.ModelView=Backbone.View.extend({
-    tplId:'',
-    initialize:function(){
-        _.bindAll(this,'render','renderTpl');
-        if(this.tplId){
-            this.tpl=$('#'+this.tplId).html();
-        }
-    },
-    render=function(){
-        this.renderTpl();
-        return this;
-    },
-    renderTpl:function(model){
-        var modelToRender=model || this.model.toJSON();
-        if(this.tpl && modelToRender){
-            var html=_.template(this.tpl,modelToRender);
-            this.html(html);
-        }
-        return this;
+Backbone.ModelView=Backbone.BasicView.extend({});
+Backbone.ModelView.prototype.tplId='';
+Backbone.ModelView.prototype.initialize=function(){
+    _.bindAll(this,'render','renderTpl');
+    if(this.tplId){
+        this.tpl=$('#'+this.tplId).html();
     }
-});
+};
+Backbone.ModelView.prototype.render=function(){
+    this.renderTpl();
+    return this;
+};
+Backbone.ModelView.prototype.renderTpl=function(model){
+    var modelToRender=model || this.model.toJSON();
+    if(this.tpl && modelToRender){
+        var html=_.template(this.tpl,modelToRender);
+        this.html(html);
+    }
+    return this;
+};
